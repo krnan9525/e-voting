e6ba0dd28310544c9e4df376c073e0baa1b03a7f
krnan9525 <sepyng@gmail.com>
2016-04-06 19:31:23 +0100

    after achool

---
+++ For_adding_candidates/README.md
@@ -0,0 +1,12 @@
+# e-voting
+
+Project dependencies:
+- Python 2.7
+- pip (https://pypi.python.org/pypi/pip)
+- Flask (pip install flask - http://flask.pocoo.org/)
+- Peewee (pip install peewee - http://docs.peewee-orm.com/en/latest/)
+- PyCrypto (pip install pycrypto)
+- sqlite
+- javascript/node js
+- 
+- For unit testing: py.test or standart unittest
---
+++ For_adding_candidates/election.sql
@@ -0,0 +1,24 @@
+--####
+--#maight be really dangerous is using drop table
+--####
+--#drop table if exists candidate;
+create table candidate (
+  id integer primary key autoincrement,
+  name text not null,
+  surname text not null,
+  party_id integer references party(party_id),
+  job text not null,
+  age integer not null,
+  address text not null
+);
+--#drop table if exists party;
+create table party (
+  party_id integer primary key autoincrement,
+  party_name text not null
+);
+--#drop table if exists ballot;
+create table ballot (
+  ballot_id integer primary key autoincrement,
+  candidate_id integer references candidate(id),
+  preference_number integer null null
+);
