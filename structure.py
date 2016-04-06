73481615098d3e98b61fe16ae5720e2d3d29bf43
krnan9525 <sepyng@gmail.com>
2016-04-06 16:56:52 +0100

    database
    1

--- structure.py
+++ structure.py
@@ -6,8 +6,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from contextlib import closing
 
 # configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
+DATABASE = 'structure.db'
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
@@ -21,12 +20,22 @@ def show_entries():
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect('structure1.db')
 
 def init_db():
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('C:\\Users\\Mark\\Desktop\\team project\\e-voting\\schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
@@ -42,3 +51,4 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+        
