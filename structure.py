9eea1e3d109c78eaed743e220959f17e82fe1de7
krnan9525 <sepyng@gmail.com>
2016-04-06 17:31:40 +0100

    database
    finish_database_building

--- structure.py
+++ structure.py
@@ -19,6 +19,12 @@ app.config.from_object(__name__)
 def before_request():
     g.db = connect_db()
 
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
@@ -35,6 +41,26 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -48,9 +74,4 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
     
 
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
         
