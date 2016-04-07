2fbc50332a296de5ca224269a907258bfcb1e105
krnan9525 <sepyng@gmail.com>
2016-04-07 15:52:26 +0100

    candidates
    1

--- For_adding_candidates/structure.py
+++ For_adding_candidates/structure.py
@@ -27,16 +27,26 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
+    cur = g.db.execute('select name, surname from candidate order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
+@app.route('/add_entry', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
+    g.db.execute('insert into candidate (title, text) values (?, ?)',
                  [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/add_party', methods=['POST'])
+def add_party():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into party (party_name) values (?)',
+                 [request.form['party_name']])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
