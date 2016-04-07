efd927701b00411e8903e73bf9cacc8300a683ca
krnan9525 <sepyng@gmail.com>
2016-04-07 16:41:24 +0100

    adding candidate function completed
    XDXD

--- For_adding_candidates/structure.py
+++ For_adding_candidates/structure.py
@@ -29,14 +29,17 @@ def teardown_request(exception):
 def show_entries():
     cur = g.db.execute('select name, surname from candidate order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    curr = g.db.execute('select party_name,party_id from party order by party_id desc')
+    parties = [dict(party_name=row[0], party_id=row[1]) for row in curr.fetchall()]
+    return render_template('show_entries.html', entries=entries, parties=parties)
 
 @app.route('/add_entry', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into candidate (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    g.db.execute('insert into candidate (name, surname, party_id, job, age, address) values (?, ?, ?, ?, ?, ?)',
+                 [request.form['name'], request.form['surname'], request.form.get('party_id'), request.form['job'],
+                  request.form['age'], request.form['address']])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
